46307564a18be05b594427d3af9d3593051fb877
Rafael Piazza <rafapcamposgm@gmail.com>
2019-05-05 02:27:55 -0300

    commitando testando

--- app/controllers/api/reports_controller.rb
+++ app/controllers/api/reports_controller.rb
@@ -57,5 +57,33 @@ module Api
 			render json: {status: 'SUCCESS', message:'Report of survivors', data:abduzidos},status: :ok
 
 		end
+
+
+
+		def alliens
+#knowing who the survivors are and how many
+			survivors = Survivor.order("name ASC");
+			total = survivors.length
+			porcent = 0.0;
+
+			abduzidos = Hash.new
+#using an empty hash to add a pair of survivors information (name,status)  and count how many abducted
+			survivors.each do |survivor|
+				if survivor.flag3==0 
+					abduzidos[survivor.name] = "Not Abducted"
+				else
+					abduzidos[survivor.name] = "Abducted"
+					porcent += 1
+				end
+			end
+#calculate percentage
+			porcent *= 100.0/total
+
+			abduzidos["Percentage of survivors abductees"] = porcent
+			abduzidos["Percentage of survivors non abductees"] = 100.0-porcent
+#and call the hash
+			render json: {status: 'SUCCESS', message:'Report of survivors', data:abduzidos},status: :ok
+
+		end
 	end
 end
